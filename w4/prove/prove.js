51899ab0841749bc404bffcaad97edb843efd70d
Renae Rogers <renae.a.rogers@gmail.com>
2026-01-31 12:46:32 -0700

    Completed modal update for coolpics gallery

---
+++ w4/prove/prove.js
@@ -0,0 +1,46 @@
+const nav = document.querySelector('nav');
+const toggle = document.querySelector('.menu-btn');
+
+toggle.addEventListener('click', () => {
+    nav.classList.toggle('active');
+});
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth > 700) {
+        nav.classList.remove('active');
+    }
+});
+
+const gallery = document.querySelector('.gallery');
+const modal = document.querySelector('dialog');
+const modalImage = modal.querySelector('img');
+const closeButton = modal.querySelector('.close-viewer');
+
+// Event listener for opening the modal
+gallery.addEventListener('click', openModal);
+
+function openModal(e) {
+    if(e.target.tagName != 'IMG') {
+        console.log("SKIP");
+        return;
+    } else {
+        const imgSrc = e.target.src.replace('-sm.jpg', '-full.jpg');
+        modalImage.src = imgSrc;
+        modal.showModal();
+    }
+    console.log(e.target);
+    
+// Code to show modal  - Use event parameter 'e'   
+    
+}
+// Close modal on button click
+closeButton.addEventListener('click', () => {
+    modal.close();
+});
+
+// Close modal if clicking outside the image
+modal.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.close();
+    }
+});
