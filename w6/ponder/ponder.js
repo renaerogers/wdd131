91d5636bdf4096f596efd7f7971665a10103c577
Renae Rogers <renae.a.rogers@gmail.com>
2026-02-10 17:22:48 -0700

    working through lists in js to html

--- w6/ponder/ponder.js
+++ w6/ponder/ponder.js
@@ -1,5 +1,5 @@
-function convert(grade) {
-    switch (grade){
+function convert(letterGrade) {
+    switch (letterGrade){
         case 'A':
             points = 4;
             break;
@@ -23,17 +23,56 @@ function convert(grade) {
 
 const words = ['watermelon', 'peach', 'apple', 'tomato', 'grape'];
 
-// ul 
+words.sort();
+const sortedFruit = words.sort();
+const fruitList = document.querySelector('#fruit-list');
 
-// let html = '';
-// words.forEach((word) => {
-//     html += `<li>${word}</li>`;
-// })
-
-// ul.innerHTML = html;
+words.forEach((word) => {
+    const li = document.createElement('li');
+    li.textContent = word;
+    fruitList.appendChild(li);
+});
 
 const students = [
-    {last: 'Andrus', first: 'Aaron'},
-    {last: 'Masa', first:'Manny'},
-    {last: 'Tanda', first: 'Tamanda'}
+    {last: 'Andrus', first: 'Aaron', grade: 'A'},
+    {last: 'Masa', first:'Manny', grade: 'B'},
+    {last: 'Tanda', first: 'Tamanda', grade: 'A'},
+    {last: 'Parker', first: 'Cody', grade: 'D'},
+    {last: 'Bluff', first:'Bethany', grade: 'F'},
+    {last: 'Elliot', first: 'Tanya', grade: 'C'},
+    {last: 'Taylor', first: 'Aaron', grade: 'B'},
+    {last: 'Allred', first:'Finn', grade: 'A'},
+    {last: 'Smith', first: 'Tamanda', grade: 'C'},
+    {last: 'Peterson', first: 'Brody', grade: 'B'},
+    {last: 'Hendrickson', first:'Reilly', grade: 'C'},
+    {last: 'Ash', first: 'Frederick', grade: 'C'},
+    {last: 'Citrus', first: 'Flora', grade: 'A'},
+    {last: 'Snow', first:'Nathan', grade: 'B'},
+    {last: 'Roberts', first: 'Olivia', grade: 'C'},
+    {last: 'Flake', first: 'Timothy', grade: 'D'},
+    {last: 'Alldridge', first:'Amanda', grade: 'F'},
+    {last: 'Kerrigan', first: 'Cora', grade: 'B'},
+    {last: 'Krout', first: 'Dakota', grade: 'B'},
+    {last: 'Coriander', first:'Toby', grade: 'A'},
+    {last: 'Yuzu', first: 'Taiga', grade: 'D'},
+    {last: 'Alderson', first: 'Sariah', grade: 'B'},
+    {last: 'Stevens', first:'Torrin', grade: 'C'},
+    {last: 'Riceberg', first: 'Adam', grade: 'A'},
 ];
+
+let html = '';
+students.forEach((student) => {
+    html += `<li style="list-style: none; padding: 5px;">${student.first} ${student.last} - (${student.grade})</li>`; 
+})
+const ul = document.querySelector('#student-list');
+ul.innerHTML = html;
+
+let average = 0;
+let points = 0;
+students.forEach((student) => {
+    points += convert(student.grade);
+})
+average = points / students.length;
+
+const averageEl = document.querySelector('#grade-average');
+averageEl.innerHTML = `<p style="padding: 5px; margin-left: 40px">Class Average: ${average.toFixed(2)} of 4.0</p>`;
