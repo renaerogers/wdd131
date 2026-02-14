9ddbf09ccaa5c4f965bab1ad547c292813c33a29
Renae Rogers <renae.a.rogers@gmail.com>
2026-02-14 16:08:57 -0700

    finished part 2 of the blog to add js content to the page, as well as fixed the formatting, adding tablet screen settings

--- blog/blog.js
+++ blog/blog.js
@@ -13,3 +13,73 @@ function addStars(span) {
 for(let i = 0; i < spans.length; i++) {
     addStars(spans[i]);
 }
+
+const articles = [
+	{
+		id: 1,
+		title: 'Septimus Heap Book One: Magyk',
+		date: 'July 5, 2022',
+		description:
+			'If you enjoy stories about seventh sons of seventh sons and magyk this is the book for you.',
+		imgSrc: 'https://upload.wikimedia.org/wikipedia/en/5/5f/Magkycover2.jpg',
+		imgAlt: 'Book cover for Septimus Heap 1',
+		ages: '10-14',
+		genre: 'Fantasy',
+		stars: '⭐⭐⭐⭐'
+	},
+	{
+		id: 2,
+		title: 'Magnus Chase Book One: Sword of Summer',
+		date: 'December 12, 2021',
+		description:
+			'The anticipated new novel by Rick Riordan. After Greek mythology (Percy Jackson), Greek/Roman (Heroes of Olympus), and Egyptian (Kane Chronicles), Rick decides to try his hand with Norse Mythology, and the end result is good.',
+		imgSrc:
+			'https://books.google.com/books/content/images/frontcover/xWuyBAAAQBAJ?fife=w300',
+		imgAlt: 'Book cover for Magnus Chase 1',
+		ages: '12-16',
+		genre: 'Fantasy',
+		stars: '⭐⭐⭐⭐'
+	},
+	{
+		id: 3,
+		title: "Belgariad Book One: Pawn of Prophecy",
+		date: "Feb 12, 2022",
+		description:
+		"A fierce dispute among the Gods and the theft of a powerful Orb leaves the World divided into five kingdoms. Young Garion, with his 'Aunt Pol' and an elderly man calling himself Wolf --a father and daughter granted near-immortality by one of the Gods -- set out on a complex mission.",
+		imgSrc:
+		"https://images-na.ssl-images-amazon.com/images/I/41ZxXA+nInL.jpg",
+		imgAlt: "Book cover for Pawn of Prophecy",
+		ages: "12-16",
+		genre: "Fantasy",
+		stars: "⭐⭐⭐⭐⭐"
+	}
+];
+
+
+const bookList = document.getElementById('book-list');
+
+articles.forEach(book => {
+    const article = document.createElement('article');
+    article.classList.add('book');
+    article.id = `book-${book.id}`;
+
+    article.innerHTML = `
+        <p>${book.date}</p>
+        <p>${book.ages}</p>
+        <p>${book.genre}</p>
+        <p><span aria-label="${book.stars.length} out of 5 stars">${book.stars}</span></p>
+        <h2>${book.title}</h2>
+        <img src="${book.imgSrc}" alt="${book.imgAlt}">
+        <p id='desc'>${book.description}</p>
+        <hr></hr>
+        `;
+
+        bookList.appendChild(article);
+    });
+
+// const spans = document.querySelectorAll('article.book p span');
+
+// function addStars(span) {
+//     const stars = span.innerText.length;
+//     span.setAttribute('aria-label', `${stars} out of 5 stars`);
+// }
