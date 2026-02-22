52324b7ff295ec43540876aca17632d36b592523
Renae Rogers <renae.a.rogers@gmail.com>
2026-02-21 23:39:42 -0700

    finished credit card code

--- w7/prove/card.js
+++ w7/prove/card.js
@@ -1,41 +1,7 @@
 
     const theForm = document.querySelector('#checkoutForm');
-    const paymentSelect = document.querySelector('#paymentMethod');
-    const creditCardContainer = document.querySelector('#creditCardNumberContainer');
-    const paypalContainer = document.querySelector('#paypalUsernameContainer');
     const creditInput = document.querySelector('#creditCardNumberContainer input');
-    const paypalInput = document.querySelector('#paypalUsernameContainer input');
- 
-function togglePaymentDetails(e) {
-    
-    // Show the container based on the selected payment method, and add the required attribute back.
-        let value = e.target.value; 
-        paypalContainer.classList.add('hide');
-        creditCardContainer.classList.add('hide');
-        paypalInput.required=false; 
-        creditInput.required=false;
-      
-        // hide and show selected payment type
-        if (value == 'creditCard') {
-          creditCardContainer.classList.remove('hide');
-          creditInput.required=true;
-        } else if (value == 'paypal') {
-          paypalContainer.classList.remove('hide');
-          paypalInput.required=true;
-        }
-
-        // Update ARIA expanded attribute
-        // aria-expanded belongs to the element that toggles visibility, not the hidden content itself.
-        if (value === 'creditCard' || value === 'paypal') {
-          paymentSelect.setAttribute("aria-expanded", "true");
-        } else {
-          paymentSelect.setAttribute("aria-expanded", "false");
-        }
-  }
-
-// attach a submit event handler to the form
-paymentSelect.addEventListener('change', togglePaymentDetails);
-
+    let paymentSelect = 'creditCard';
 //validations and errors
 
 function displayError(msg) {
@@ -66,7 +32,6 @@ function submitHandler(event) {
       } else if (!isCardNumberValid(cardNum)) {
         errorMsg += 'Card number is not valid\n';
       }
-    
     //check date
     const expYear = Number(document.querySelector('#year').value);   
     const expMonth = Number(document.querySelector('#month').value);
@@ -75,6 +40,11 @@ function submitHandler(event) {
     if (2000 + expYear < currentDate.getFullYear() || (2000 + expYear === currentDate.getFullYear() && expMonth <= (currentDate.getMonth()))
     ) {
         errorMsg += 'Card is expired\n';
+    }
+    
+    const cvc = document.querySelector('#cvcCvv').value.trim();
+    if (!/^\d{3}$/.test(cvc)) {
+        errorMsg += 'CVC/CVV must be 3 digits\n';
     }
   }
 
