1ae85b35eab7cbee4a240bdd1e393905e4865489
Renae Rogers <renae.a.rogers@gmail.com>
2026-02-26 09:36:00 -0700

    using objects prove completed

--- w8/ponder/objects.js
+++ w8/ponder/objects.js
@@ -3,9 +3,9 @@ const aCourse = {
     name: 'Javascript Language',
     logo: 'images/js-logo.png',
     sections: [
-        { sectionNum: 1, roomNum: 'STC 353', enrolled: 26, days: 'TTh', instructor: 'Bro T', class: "WDD131"},
-        { sectionNum: 2, roomNum: 'STC 347', enrolled: 28, days: 'TTh', instructor: 'Sis A', class: "WDD131"},
-        { sectionNum: 3, roomNum: 'STC 353', enrolled: 30, days: 'MW', instructor: 'Bro K', class: "WDD131"},
+        { sectionNum: 1, roomNum: 'STC 353', enrolled: 26, days: 'TTh', instructor: 'Bro K', class: "WDD131"},
+        { sectionNum: 2, roomNum: 'STC 347', enrolled: 18, days: 'MW', instructor: 'Bro W', class: "CSE199R"},
+        { sectionNum: 3, roomNum: 'STC 353', enrolled: 30, days: 'TTh', instructor: 'Bro B', class: "CSE111"},
     ],
     enrollStudent: function (sectionNum) {
         // find the right section...Array.findIndex will work here
@@ -37,15 +37,21 @@ function sectionTemplate(section) {
         <td>${section.class}</td></tr>`
 }
 
-function renderSections(sections) {
-const html = sections.map(sectionTemplate);
-document.querySelector("#sections").innerHTML = html.join("");
+const totalEnrolled = document.querySelector("#totalEnrollment");
+function renderTotalEnrollment(sections) {
+    const total = sections.reduce((total, section) => total + section.enrolled, 0);
+    totalEnrolled.innerHTML = `Total Enrollment: ${total} students`;
 }
 
+function renderSections(sections) {
+    const html = sections.map(sectionTemplate);
+    document.querySelector("#sections").innerHTML = html.join("");
+
+    renderTotalEnrollment(sections);
+}
 renderSections(aCourse.sections);
 
 document.querySelector("#enrollStudent").addEventListener("click", function () {
         const sectionNum = document.querySelector("#sectionNumber").value;
         aCourse.enrollStudent(sectionNum);
 });
-
